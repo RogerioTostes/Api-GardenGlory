e527b5217ee125824428c7538ca4ae0ec907690b
RogerioTostes <rogertotes@yahoo.com.br>
2020-07-30 15:41:22 -0300

    initial commit

---
+++ Api-GardenGlory/Controllers/GardenGloryController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Api_GardenGlory.Models;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace Api_GardenGlory.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GardenGloryController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Passaro>), (int)HttpStatusCode.OK)]
+        public IEnumerable<Passaro> Get(
+            [FromServices] IConfiguration configuration)
+        {
+            using (SqlConnection conexao = new SqlConnection(
+                configuration.GetConnectionString("GardenGlory")))
+            {
+                return conexao.Query<Passaro>(
+                    "SELECT * FROM dbo.Passaro");
+            }
+        }
+
+        [HttpGet("{Anel}")]
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Get))]
+        public ActionResult<Passaro> Get(
+            [FromServices] IConfiguration configuration,
+            string indicador)
+        {
+            Passaro resultado = null;
+
+            using (SqlConnection conexao = new SqlConnection(
+                configuration.GetConnectionString("GardenGlory")))
+            {
+                resultado = conexao.QueryFirstOrDefault<Passaro>(
+                    "SELECT * FROM dbo.Indicadores " +
+                    "WHERE Sigla = @siglaIndicador",
+                    new { siglaIndicador = indicador });
+            }
+
+            if (resultado != null)
+                return resultado;
+            else
+            {
+                return NotFound(
+                    new
+                    {
+                        Mensagem = "Indicador inválido ou inexistente."
+                    });
+            }
+        }
+    }
+}
---
+++ Api-GardenGlory/Models/GardenGlory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Api_GardenGlory.Models
+{
+    public class Passaro
+    {
+        public int Anel { get; set; }
+        public int Criador { get; set; }
+        public char Sexo { get; set; }
+        public string Cor { get; set; }
+        public DateTime AnoNascimento { get; set; }
+        public string Obs { get; set; }
+
+    }
+    public class Gaiola
+    {
+        public int NumeroOrdem { get; set; }
+        public int Anel { get; set; }
+        public int QtdOvos { get; set; }
+        public int Total { get; set; }
+        public int Ferteis { get; set; }
+        public DateTime Incubacao { get; set; }
+        public DateTime Nascimento { get; set; }
+        public int Nascidos { get; set; }
+        public int Mortos { get; set; }
+        public int Criados { get; set; }
+        public string Aneis { get; set; }
+
+    }
+}
